199cfecbe0a14db98c48f6e54896935d223123b9
Vaascoo <vcvasco1@gmail.com>
2024-03-26 02:42:01 +0000

    Enable zram swap

--- hosts/chord/hw.nix
+++ hosts/chord/hw.nix
@@ -63,7 +63,8 @@
     fsType = "vfat";
   };
 
-  swapDevices = [{device = "/dev/disk/by-label/swap";}];
+  # swapDevices = [{device = "/dev/disk/by-label/swap";}];
+  zramSwap.enable = true;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
