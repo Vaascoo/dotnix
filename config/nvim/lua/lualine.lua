a1c6c499cd9773ca117562ccf8867d364f7fbda5
Vaascoo <vcvasco1@gmail.com>
2024-08-02 05:15:47 +0100

    Update neovim config

--- config/nvim/lua/lualine.lua
+++ config/nvim/lua/lualine.lua
@@ -1,7 +1,8 @@
+require('')
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox',
+    theme = 'tomorrow-night',
     component_separators = { left = '', right = '' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {
